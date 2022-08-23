04c9b5359c91a5ca6d9da547ef153ef2dadb177c
jacoong <yuh0812@gmail.com>
2022-08-23 14:38:32 +0900

    first commit of today

--- Script/music.js
+++ Script/music.js
@@ -8,7 +8,7 @@ function onYouTubeIframeAPIReady() {
     player = new YT.Player("player", {
         height: "300",
         width: "300",
-        videoId: "FUeXStr48qY",
+        videoId: "HWCJI7mCHZE",
         playerVars: {autoplay: 0},
         events: {},
         
@@ -21,19 +21,19 @@ const currentSongName = document.querySelector("#playList");
 
 const PLAYLIST_ID = [
   {
-    playListName: "Fall Out Boy - The Last Of The Real Ones",
+    playListName: "[1]Fall Out Boy - The Last Of The Real Ones",
     videoId: "HWCJI7mCHZE",
   },
   {
-    playListName: "같이해요 로파이 노동요",
+    playListName: "[2]같이해요 로파이 노동요",
     videoId: "O4WRwIt5hA4",
   },
   {
-    playListName: "맑은 아침을 맞이하는 기분 좋은 연주곡",
+    playListName: "[3]맑은 아침을 맞이하는 기분 좋은 연주곡",
     videoId: "Tt4X3Dy6RB8",
   },
   {
-    playListName: "창문을 열고, 여름 햇살을 맞으며 듣기 좋은 피아노 연주곡",
+    playListName: "[4]창문을 열고, 여름 햇살을 맞으며 듣기 좋은 피아노 연주곡",
     videoId: "xUIUzSCQJks",
   },
 ];
@@ -53,28 +53,46 @@ function handlePauseBtnClick(){
 } 
 
 function handleNextBtnClick(){
-if (currentPlay < PLAYLIST_ID.length-1){
+  currentPlay += 1
+if (currentPlay <= PLAYLIST_ID.length-1){
   player.loadVideoById(PLAYLIST_ID[currentPlay].videoId);
   currentSongName.innerText =`${PLAYLIST_ID[currentPlay].playListName}`
-  currentPlay = currentPlay+1
 console.log(currentPlay);
 }
   
-if (currentPlay >=(PLAYLIST_ID.length-1)){
+if (currentPlay >(PLAYLIST_ID.length-1)){
   currentPlay = 0;
   player.loadVideoById(PLAYLIST_ID[currentPlay].videoId);
   currentSongName.innerText =`${PLAYLIST_ID[currentPlay].playListName}`
+  console.log(currentPlay);
 }
-} 
+}
 
+function handleBeforeBtnClick(){
+    currentPlay-=1
+   
+  if (currentPlay < PLAYLIST_ID.length && currentPlay > 0){
+    player.loadVideoById(PLAYLIST_ID[currentPlay].videoId);
+    currentSongName.innerText =`${PLAYLIST_ID[currentPlay].playListName}`
+    console.log(currentPlay);
+  }
 
+  if (currentPlay ===-1){
+    currentPlay = PLAYLIST_ID.length -1
+    player.loadVideoById(PLAYLIST_ID[currentPlay].videoId);
+    currentSongName.innerText =`${PLAYLIST_ID[currentPlay].playListName}`
+    console.log("aa");
+  }
+}
 
 
 
 const playButton = document.querySelector("#play");
 const pauseButton = document.querySelector("#pause");
 const nextButton = document.querySelector("#next");
+const beforeButton = document.querySelector("#before");
 
+beforeButton.addEventListener("click", handleBeforeBtnClick);
 playButton.addEventListener("click", handlePlayBtnClick);
 pauseButton.addEventListener("click", handlePauseBtnClick);
 nextButton.addEventListener("click", handleNextBtnClick);
